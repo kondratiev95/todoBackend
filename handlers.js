8e5ae057e85c5e3f8b502fb717d9f2d6d9c8e028
Vladimir <volodymyrkondratiev@devico.io>
2022-01-21 10:27:37 +0200

    finish native node

--- handlers.js
+++ handlers.js
@@ -1,8 +1,13 @@
 import { ObjectId } from "mongodb";
 
-export const updateUI = async (todosCollection, res) => {
+export const getTodos = async (todosCollection, res) => {
     const array = await todosCollection.find({}).toArray();
-    res.end(JSON.stringify(array));
+    if(array) {
+        res.end(JSON.stringify(array));
+    } else {
+        throw new Error('Could not get data');
+    }
+        
 }
 export const addData = async (todosCollection, body, res) => {
     const newTodoItem = {
@@ -10,36 +15,36 @@ export const addData = async (todosCollection, body, res) => {
         value: JSON.parse(body).trim()
     }
     await todosCollection.insertOne(newTodoItem);
-    updateUI(todosCollection, res);
+    getTodos(todosCollection, res);
 }
 export const removeItem = async (todosCollection, body, res) => {
     const id = new ObjectId(JSON.parse(body));
     await todosCollection.deleteOne({_id: id});
-    updateUI(todosCollection, res);
+    getTodos(todosCollection, res);
 }
 export const toggleItem = async (todosCollection, body, res) => {
     const id = new ObjectId(JSON.parse(body));
     const currentItem = await todosCollection.findOne({_id: id});
     await todosCollection.findOneAndUpdate({_id: id}, {$set: {completed: !currentItem.completed}})
-    updateUI(todosCollection, res);
+    getTodos(todosCollection, res);
 }
 export const toggleAll = async (todosCollection, body, res) => {
     if(JSON.parse(body)) {
         await todosCollection.updateMany({}, {$set: { completed: false}});
-        updateUI(todosCollection, res);
+        getTodos(todosCollection, res);
     } else {
         await todosCollection.updateMany({}, {$set: { completed: true}});
-        updateUI(todosCollection, res);
+        getTodos(todosCollection, res);
     }
 }
 export const deleteCompleted = async (todosCollection, res) => {
     await todosCollection.deleteMany({completed : true});
-    updateUI(todosCollection, res);
+    getTodos(todosCollection, res);
 }
 export const editTodo = async (todosCollection, body, res) => {
     const obj = JSON.parse(body);
     const id = new ObjectId(obj.id);
     await todosCollection.updateOne({_id: id}, {$set: { value: obj.value}});
-    updateUI(todosCollection, res);
+    getTodos(todosCollection, res);
 }
  
--- server.js
+++ server.js
@@ -1,75 +1,78 @@
 import http from "http";
 import { MongoClient } from "mongodb";
-import  { ENDPOINTS } from "./apiConstants.js";
+import { ENDPOINTS } from "./apiConstants.js";
 import { port, hostname, dbName, uri } from "./config.js";
-import { 
-  addData, 
-  removeItem, 
-  toggleItem, 
-  toggleAll, 
-  deleteCompleted, 
+import {
+  addData,
+  removeItem,
+  toggleItem,
+  toggleAll,
+  deleteCompleted,
   editTodo,
-  updateUI
+  getTodos,
 } from "./handlers.js";
 import { setHeaders } from "./headers.js";
 
 let db = null;
 
-const client = new MongoClient(uri,{ useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 async function connectToDB() {
   try {
     await client.connect();
     db = client.db(dbName);
-  } catch(err) {
-    console.log('failed to connect DB', err.stack);
+  } catch (err) {
+    console.log("failed to connect DB", err.stack);
   }
 }
 
 const server = http.createServer();
 
-server.on('request', (req, res) => {
+server.on("request", (req, res) => {
   let body = "";
   setHeaders(res);
   const todosCollection = db.collection("todos");
 
-  if(req.method === "GET") {
-    updateUI(todosCollection, res);
+  if (req.method === "GET") {
+    getTodos(todosCollection, res);
   }
 
-  req.on('data', chunk => {
+  req.on("data", (chunk) => {
     body += chunk;
 
-    if(req.method === "POST") {
-      switch(req.url) {
+    if (req.method === "POST") {
+      switch (req.url) {
         case ENDPOINTS.addData:
-          addData(todosCollection,body,res);
+          addData(todosCollection, body, res);
           break;
         case ENDPOINTS.removeItem:
-          removeItem(todosCollection,body,res);
+          removeItem(todosCollection, body, res);
           break;
         case ENDPOINTS.toggleItem:
-          toggleItem(todosCollection,body,res);
+          toggleItem(todosCollection, body, res);
           break;
         case ENDPOINTS.toggleAll:
-          toggleAll(todosCollection,body,res);
+          toggleAll(todosCollection, body, res);
           break;
         case ENDPOINTS.deleteCompleted:
           deleteCompleted(todosCollection, res);
           break;
         case ENDPOINTS.editTodo:
-          editTodo(todosCollection,body,res);
+          editTodo(todosCollection, body, res);
           break;
         default:
-          res.writeHead(404, 'Not found', {'content-type' : 'text/plain'});
+          res.writeHead(404, "Not found", { "content-type": "text/plain" });
           res.end();
           break;
       }
     }
-  })
-  req.on('error', () => {
-    res.writeHead(400, 'Could not get data', {'content-type' : 'text/plain'});
+  });
+  req.on("error", () => {
+    res.writeHead(500, "Could not get data", { "content-type": "text/plain" });
     res.end();
-  })
+  });
 });
 
 connectToDB();
